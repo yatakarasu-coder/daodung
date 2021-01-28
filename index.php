acb6d2ea5d4c3c9d2f67cfa6b10d663310e9f188
Student <std@aahn02.edu>
2021-01-28 21:22:41 +0700

    add

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Personal Information</title>
+	<meta charset="utf-8"/>
+</head>
+<body style="background-color: #a1d3d6; background-image: url(https://www.pbs.org/wgbh/nova/media/images/bfcppfu.width-800.png);background-repeat: no-repeat; background-size: cover; color: white">
+
+	<h1 style="color: white;font-size: 60px; text-align: center">PERSONAL INFORMATION</h1>
+	<p style="text-align:center;"><img src="https://scontent.fhan2-1.fna.fbcdn.net/v/t1.0-9/131670424_818285338954824_8763931980069885091_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=17uFIEa0UiEAX8GqoJn&_nc_ht=scontent.fhan2-1.fna&oh=e8c9f5e3ecc395b900304d61f57bcde0&oe=6038DE8C" 
+	alt="Logo" style="width: 256px;height: 256px"></p>
+	<h2 style="color: #ff3d40;font-size: 40px;text-align: center;text-decoration-style: bold;">ABOUT ME</h2>
+<p style="text-align: center;font-family: Arial;font-size: 30px">Name: ĐÀO MẠNH DŨNG</p>
+<p style="text-align: center;font-family: Arial;font-size: 30px">Age: 18</p>
+<p style="text-align: center;font-family: Arial;font-size: 30px">Student of Post National University of Civil Engineering</p>
+<p style="text-align: center;font-family: Arial;font-size: 30px">Profession: Software Engineer</p>
+<p style="text-align: center;font-family: Arial;font-size: 30px">E-Address: dung47765@nuce.edu.vn</p>
+<p style="text-align: center;font-family: Arial;font-size: 30px">Address: VĨNH PHÚC</p>
+<a href="https://www.facebook.com/include.daodung/" target="center" style="color: yellow"><p style="text-align: center;font-family: Arial;color: red;font-size: 35px">Visit my Facebook personal page!</a></p>
+<p style="text-align: center;font-family: Arial;font-size: 15px">&#128151 &#128151 &#128151</p>
+
+
+
+
+
+
+</body>
+</html>
