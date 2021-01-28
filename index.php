4f1057123fae78fea67801573ce2ad4b9fbd2cd7
Student <std@aahn02.edu>
2021-01-28 21:51:00 +0700

    second commit

--- index.php
+++ index.php
@@ -6,10 +6,9 @@
 </head>
 <body style="background-color: #a1d3d6; background-image: url(https://www.pbs.org/wgbh/nova/media/images/bfcppfu.width-800.png);background-repeat: no-repeat; background-size: cover; color: white">
 
-	<h1 style="color: white;font-size: 60px; text-align: center">PERSONAL INFORMATION</h1>
+	<h2 style="color: white;font-size: 60px; text-align: center">PERSONAL INFORMATION</h2>
 	<p style="text-align:center;"><img src="https://scontent.fhan2-1.fna.fbcdn.net/v/t1.0-9/131670424_818285338954824_8763931980069885091_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=17uFIEa0UiEAX8GqoJn&_nc_ht=scontent.fhan2-1.fna&oh=e8c9f5e3ecc395b900304d61f57bcde0&oe=6038DE8C" 
 	alt="Logo" style="width: 256px;height: 256px"></p>
-	<h2 style="color: #ff3d40;font-size: 40px;text-align: center;text-decoration-style: bold;">ABOUT ME</h2>
 <p style="text-align: center;font-family: Arial;font-size: 30px">Name: ĐÀO MẠNH DŨNG</p>
 <p style="text-align: center;font-family: Arial;font-size: 30px">Age: 18</p>
 <p style="text-align: center;font-family: Arial;font-size: 30px">Student of National University of Civil Engineering</p>
